5e181a9439d55d8145b304b1f2b4999b154b0a72
Mirdev <31503196+Mirdev@users.noreply.github.com>
2021-08-01 16:57:44 +0900

    Create 6_FND_showNumberDec.ino

---
+++ 6_FND_showNumberDec.ino
@@ -0,0 +1,23 @@
+#include <TM1637Display.h>
+
+#define CLK 8
+#define DIO 9
+
+TM1637Display disp(CLK, DIO);
+
+void setup(){
+  disp.setBrightness(0);
+}
+
+void loop(){
+//  showNumberDec(int num, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0)
+//  both length and pos are from left, diffrent in numbers (1~4), (0~3)
+  disp.showNumberDec(7777); // 7777
+  delay(1000);
+  disp.showNumberDec(234, false, 3, 1); // 7 234
+  delay(1000);
+  disp.showNumberDec(23, true, 3); // 023 4
+  delay(1000);
+  disp.showNumberDec(2, false, 3); // 02 2 4
+  delay(1000);
+}
